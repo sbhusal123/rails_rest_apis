1f656bab9750a2f1fd0c59fbcfaa559cf2e6e8f6
Surya Bhusal <surya@fetch.ly>
2023-03-14 17:01:22 +0545

    Exception handelling from controllers with rescue_from

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def index
     render json: Book.all
   end
@@ -17,6 +18,7 @@ class BooksController < ApplicationController
     head :no_content
   end
 
+  private
   def book_params
     params.require(:book).permit(:title, :author)
   end
